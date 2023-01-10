b3e051c61b9858ef229a597af2650b1141d3a1cf
paradox356 <iyerdhanush4@gmail.com>
2023-01-10 19:25:10 +0000

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,38 @@
 import "./styles.css";
 import { useState } from "react";
 
-var [time, setTime] = useState(new Date().toLocaleTimeString());
+const emoDic = {
+  "😀": "Grinning Face",
+  "😂": "Face with Tears of Joy",
+  "😍": "Smiling Face with Heart-Eyes",
+  "😘": "Face Blowing a Kiss",
+  "😐": "Neutral Face",
+};
+const arrOfEmo = Object.keys(emoDic);
+
+// function showAllArrElements(prop) {
+//   for (let i = 0; i < n; i++) {
+//     return <h2>{prop[i]}</h2>;
+//   }
+// }
 export default function App() {
+  var [emostate, setEmostate] = useState("");
   return (
     <div className="App">
-      <h1>Hello, world!</h1>
-      <h2>{time}</h2>
+      <h1>Emoji Dictionary</h1>
+      <input
+        onChange={(e) => {
+          arrOfEmo.map((x) => {
+            if (e.target.value != x) {
+              setEmostate("Emoji Not Found");
+            } else {
+              setEmostate(e.target.value);
+            }
+          });
+        }}
+      />
+      <h3>{emoDic[emostate]}</h3>
+      {arrOfEmo.map((x) => x)}
     </div>
   );
 }
