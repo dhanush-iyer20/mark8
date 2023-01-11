cb9702c6c5e54bbd8cae6963dc97ab5a75b858f7
paradox356 <iyerdhanush4@gmail.com>
2023-01-11 12:28:21 +0000

    Final update : app is completed with all css and functions

--- src/App.js
+++ src/App.js
@@ -13,27 +13,46 @@ const arrOfEmo = Object.keys(emoDic);
 export default function App() {
   var [emostate, setEmostate] = useState("");
   var [error, setError] = useState("");
+
+  function checkIfEmoInArray(e) {
+    console.log(e.target.value);
+    for (let i = 0; i < arrOfEmo.length; i++) {
+      if (e.target.value == arrOfEmo[i]) {
+        return setEmostate(e.target.value), setError("");
+      }
+    }
+    return setError("error in finding emoji"), setEmostate("");
+  }
+
   return (
     <div className="App">
-      <h1>Emoji Dictionary</h1>
+      <div className="content">
+        <h1 className="aniText">Emoji Dictionary</h1>
+      </div>
       <input
+        className="inputelement"
         onChange={(e) => {
           //case of empty input box
           if (e.target.value == "") {
             return setError(""), setEmostate("");
           }
           //for loop to check whether the input lies in the keys array
-          for (let i = 0; i < arrOfEmo.length; i++) {
-            if (e.target.value == arrOfEmo[i]) {
-              return setEmostate(e.target.value), setError("");
-            }
-          }
-          return setError("error in finding emoji"), setEmostate("");
+          checkIfEmoInArray(e);
         }}
+        placeholder="type an emoji or click below"
       />
-      <h3>{emoDic[emostate]}</h3>
+      <h3 className="mainText">{emoDic[emostate]}</h3>
       <h3>{error}</h3>
-      {arrOfEmo.map((x) => x)}
+      {arrOfEmo.map((x) => (
+        <button
+          className="emoBtns"
+          key={x}
+          value={x}
+          onClick={(e) => checkIfEmoInArray(e)}
+        >
+          {x}
+        </button>
+      ))}
     </div>
   );
 }
